c78f076e2d9971d7208021ca11c5fbb740e6e89d
Akshaj Verma <akshajverma7@gmail.com>
2022-11-25 23:00:26 +0100

    Update test.

--- src/satsolver/hypothesis_test.py
+++ src/satsolver/hypothesis_test.py
@@ -14,28 +14,52 @@ class HypothesisTest:
 
 
 if __name__ == "__main__":
-    hyp_test = HypothesisTest(csv_file_path="./data/output/complete_stats_freek.csv")
+    hyp_test = HypothesisTest(csv_file_path="./data/output/results.csv")
     df = hyp_test.df
-    df_baseline = df[df["algorithm"] == "Baseline"]["backtracks"]
-    df_heuristic_1 = df[df["algorithm"] == "Heuristic 1"]["backtracks"]
-    df_heuristic_2 = df[df["algorithm"] == "Heuristic 2"]["backtracks"]
+    df["prefilled_boxes"] = (
+        df["sudoku_file"].str.split(".").str[0].str.split("_").str[-1]
+    ).astype(int)
 
-    labels = ["baseline", "heuristic 1", "heuristic 2"]
-    data = [df_baseline, df_heuristic_1, df_heuristic_2]
+    # t-test between different algos
+    # df_baseline = df[df["algorithm"] == "Baseline"]["backtracks"]
+    # df_heuristic_1 = df[df["algorithm"] == "Heuristic 1"]["backtracks"]
+    # df_heuristic_2 = df[df["algorithm"] == "Heuristic 2"]["backtracks"]
 
-    labels_combo = combinations(labels, 2)
-    data_combo = combinations(data, 2)
+    algos = ["Baseline", "Heuristic 1", "Heuristic 2"]
+    multi_solution_prefilled_vals = [4, 9, 15]
+    single_solution_prefilled_vals = [18]
 
-    results_df = pd.DataFrame(columns=["Data 1", "Data 2", "Statistic", "P-Value"])
     results_list = []
-    for lbl, d in zip(labels_combo, data_combo):
-        print(lbl)
-        statistic, pvalue = stats.ttest_ind(a=d[0], b=d[1], equal_var=False)
-        print(statistic, pvalue)
-        results_list.append([lbl[0], lbl[1], statistic, pvalue])
+    for a in algos:
+        df_algo = df[df["algorithm"] == a]
+        for s in single_solution_prefilled_vals:
+            df_algo_backtracks_singlesol = df_algo[df_algo["prefilled_boxes"] == s][
+                "backtracks"
+            ]
+
+            for m in multi_solution_prefilled_vals:
+                df_algo_backtracks_multisol = df_algo[df_algo["prefilled_boxes"] == m][
+                    "backtracks"
+                ]
+
+                print(a, s, m)
+                statistic, pvalue = stats.ttest_ind(
+                    a=df_algo_backtracks_singlesol,
+                    b=df_algo_backtracks_multisol,
+                    equal_var=False,
+                )
+                print(statistic, pvalue)
+                results_list.append([a, s, m, statistic, pvalue])
 
     results_df = pd.DataFrame(
-        results_list, columns=["Data 1", "Data 2", "Statistic", "P-Value"]
+        results_list,
+        columns=[
+            "Algorithm",
+            "Unique Solution Prefilled Values",
+            "Multi Solution Prefilled Values",
+            "T-Value",
+            "P-Value",
+        ],
     )
 
     results_df.to_csv("./data/output/t-test.csv", index=False)
